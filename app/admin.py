13d2e7c32fea2686c38da21b18a0b4baf5831d95
atomyongya <atom.yongya@truenary.com>
2023-07-04 22:28:11 +0545

    feat: create hotel page model

--- app/admin.py
+++ app/admin.py
@@ -6,6 +6,9 @@ from .models.detail.detail_model import DetailModel
 from .models.detail.component.customer_service_model import CustomerServiceModel
 from .models.detail.component.detail_info_model import DetailInfoModel
 from .models.detail.component.itinary_model import ItinaryModel
+from .models.hotel.hotel_model import HotelModel
+from .models.hotel.component.hotel_service_model import HotelServiceModel
+from .models.user_reviews.user_review_model import UserReviewModel
 
 admin.site.register([
     HomeModel,
@@ -15,4 +18,7 @@ admin.site.register([
     CustomerServiceModel,
     DetailInfoModel,
     ItinaryModel,
+    HotelModel,
+    HotelServiceModel,
+    UserReviewModel,
 ])
