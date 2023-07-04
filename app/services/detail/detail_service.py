b8d58c6e3a212fd72ba0853ffc0b956d149f12db
atomyongya <atom.yongya@truenary.com>
2023-07-04 10:26:50 +0545

    feat: detail page design and feature is complete

--- app/services/detail/detail_service.py
+++ app/services/detail/detail_service.py
@@ -1,17 +1,21 @@
+from app.services.detail.component.kathmandu_tour_destination_service import KathmanduTourService
+from app.services.detail.component.popular_destination_detail_service import PopularDestinationService
+
+
 class DetailService:
     def get_data(
         self,
-        popular_destination_object,
-        kathmandu_tour_object,
+        popular_destination_object = None,
+        kathmandu_tour_object = None,
     ):
         if popular_destination_object != None:
-            return {
-                'id': popular_destination_object.id,
-            }
+            popular_destination_service = PopularDestinationService()
+            return popular_destination_service.getData(
+                popular_destination_object = popular_destination_object,
+            )
 
         else:
-            return {
-                'id': kathmandu_tour_object.id,
-            }
-
-        return data
+            kathmandu_tour_service = KathmanduTourService()
+            return kathmandu_tour_service.getData(
+                kathmandu_tour_object = kathmandu_tour_object,
+            )
