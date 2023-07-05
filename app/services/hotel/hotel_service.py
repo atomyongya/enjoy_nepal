981d94af8ce422a8e2ab9923d679811f1b0e1f29
atomyongya <atom.yongya@truenary.com>
2023-07-05 05:57:06 +0545

    feat: successfully filter hotel detail data

--- app/services/hotel/hotel_service.py
+++ app/services/hotel/hotel_service.py
@@ -4,9 +4,11 @@ from app.models.hotel.hotel_model import HotelModel
 
 class HotelService:
     def getData(self, hotel_object: HotelModel):
+        title = hotel_object.title
         hotel_details = hotel_object.hotel_detail.all()
 
         data = {
+            'title': title,
             'hotel_details': hotel_details,
         }
 
--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from app.views.hotel_detail_view import HotelDetailView
 
 from app.views.hotel_view import HotelView
 from .views.home_view import HomeView
@@ -6,6 +7,7 @@ from .views.detail_view import DetailView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('hotel', HotelView.as_view(), name='hotel'),
     path('detail-layout', DetailView.as_view(), name='detail_layout'),
+    path('hotel', HotelView.as_view(), name='hotel'),
+    path('hotel_detail', HotelDetailView.as_view(), name='hotel_detail'),
 ]
