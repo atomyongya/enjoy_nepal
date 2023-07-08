bfda463aa8ead3930be3b765517813588f2f41f2
atomyongya <atom.yongya@truenary.com>
2023-07-08 06:48:17 +0545

    feat: apply design to most of the pages

--- app/admin.py
+++ app/admin.py
@@ -11,6 +11,8 @@ from app.models.things_to_do.component.treak_model import TreakModel
 from app.models.things_to_do.things_to_do_model import ThingsToDoModel
 from app.models.vacation_rentals.component.vacation_rentals_detail_model import VacationRentalsDetailModel
 from app.models.vacation_rentals.vacation_rentals_model import VacationRentalsModel
+from app.services.things_to_do.things_to_do_detail.things_to_do_detail_service import ThingsToDoDetailService
+from app.views.things_to_do_detail_view import ThingsToDoDetailView
 from .models.home.home_model import HomeModel
 from .models.home.component.kathmandu_tour_model import KathmanduTourModel
 from .models.home.component.popular_destination_model import PopularDestinationModel
