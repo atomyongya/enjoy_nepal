57d54cdb23d22fb790f58214c6feb3a822e5ec2b
atomyongya <atom.yongya@truenary.com>
2023-07-08 18:11:16 +0545

    fat: apply khalti payment

--- alltrails/urls.py
+++ alltrails/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('app.urls')),
     path('', include('authentication.urls')),
     path('accounts/', include('allauth.urls')),
+    path('khalti/', include('khalti.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
